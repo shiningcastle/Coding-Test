55dcb166d5ac168aa16575e7fa5e1a0f55f01ab4
shiningcastle <54968463+shiningcastle@users.noreply.github.com>
2021-06-06 00:13:02 +0900

    Update 소수 만들기.py

--- 프로그래머스/소수 만들기.py
+++ 프로그래머스/소수 만들기.py
@@ -4,7 +4,7 @@ def primeNumber(n):
     if n == 0 or n == 1:
         return False
     
-    for i in range(2, n):
+    for i in range(2, (n // 2) + 1):
         if n % i == 0:
             return False
         
