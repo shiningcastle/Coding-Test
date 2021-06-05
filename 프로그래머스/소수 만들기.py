1f69630a559644b540e74e124bbbed6361c672c5
shiningcastle <54968463+shiningcastle@users.noreply.github.com>
2021-06-06 00:03:11 +0900

    Create 소수 만들기.py

---
+++ 프로그래머스/소수 만들기.py
@@ -0,0 +1,21 @@
+from itertools import combinations
+# 소수 판별 함수
+def primeNumber(n):
+    if n == 0 or n == 1:
+        return False
+    
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+        
+    return True
+    
+def solution(nums):
+    # 3개 숫자 조합 모든 경우의 수 튜플 리스트로
+    case = list(combinations(nums, 3))
+    result = 0
+    for t in case:
+        if primeNumber(sum(t)):
+            result += 1
+    
+    return result
