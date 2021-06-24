5319407bdaf27e299cce9b575190475f760bf760
shiningcastle <54968463+shiningcastle@users.noreply.github.com>
2021-06-24 23:22:10 +0900

    Update 행렬 테두리 회전하기.py

--- 프로그래머스/행렬 테두리 회전하기.py
+++ 프로그래머스/행렬 테두리 회전하기.py
@@ -1,38 +1,38 @@
 def solution(rows, columns, queries):
-
+    matrix = [[0 for _ in range(columns)] for _ in range(rows)]
     answer = []
-    array = [[0 for col in range(columns)] for row in range(rows)]
-    t = 1
-    for row in range(rows):
-        for col in range(columns):
-            array[row][col] = t
-            t += 1
-
-    for x1,y1,x2,y2 in queries:
-        tmp = array[x1-1][y1-1]
-        mini = tmp
-
-        for k in range(x1-1,x2-1):
-            test = array[k+1][y1-1]
-            array[k][y1-1] = test
-            mini = min(mini, test)
-
-        for k in range(y1-1,y2-1):
-            test = array[x2-1][k+1]
-            array[x2-1][k] = test
-            mini = min(mini, test)
-
-        for k in range(x2-1,x1-1,-1):
-            test = array[k-1][y2-1]
-            array[k][y2-1] = test
-            mini = min(mini, test)
-
-        for k in range(y2-1,y1-1,-1):
-            test = array[x1-1][k-1]
-            array[x1-1][k] = test
-            mini = min(mini, test)
-
-        array[x1-1][y1] = tmp
-        answer.append(mini)
-
+    # 행렬에 1씩 증가하는 수 채워넣음
+    for i in range(rows):
+        for j in range(columns):
+            matrix[i][j] = i * columns + j + 1
+            
+    for x1, y1, x2, y2 in queries:
+        # 시작점은 첫번째 for문에서 값이 사라지므로 보관
+        start = matrix[x1-1][y1-1]
+        # 시작점은 옮기는 수들의 min 연산에 포함되지 않으므로 포함위해 초기화
+        minimum = start
+        # 왼쪽 변
+        for i in range(x1, x2):
+            number = matrix[i][y1-1]
+            matrix[i-1][y1-1] = number
+            minimum = min(minimum, number)
+        # 아래쪽 변
+        for j in range(y1, y2):
+            number = matrix[x2-1][j]
+            matrix[x2-1][j-1] = number
+            minimum = min(minimum, number)
+        # 오른쪽 변    
+        for i in range(x2, x1, -1):
+            number = matrix[i-2][y2-1]
+            matrix[i-1][y2-1] = number
+            minimum = min(minimum, number)
+        # 위쪽 변
+        for j in range(y2, y1, -1):
+            number = matrix[x1-1][j-2]
+            matrix[x1-1][j-1] = number
+            minimum = min(minimum, number)
+        # 시작점은 아직 옮기지 못했으므로 여기서 반영     
+        matrix[x1-1][y1] = start
+        answer.append(minimum)
+    
     return answer
