c25b9d32049e93c5958bc8b9e1c3bd4a9cb7e68e
shiningcastle <54968463+shiningcastle@users.noreply.github.com>
2021-07-19 15:30:24 +0900

    Create PermMissingElem.java

---
+++ Codility/PermMissingElem.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != i+1)
+                return i+1;
+        }
+        return A.length+1;
+    }
+}
