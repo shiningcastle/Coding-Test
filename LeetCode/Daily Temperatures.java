e09ca82b9b8ac2b233bdb3de792eb2303da4137b
hs <shiningcastle.dev@gmail.com>
2021-08-18 02:25:47 +0900

    Update Daily Temperatures.java

--- LeetCode/Daily Temperatures.java
+++ LeetCode/Daily Temperatures.java
@@ -3,13 +3,13 @@ import java.util.Stack;
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] answer = new int[temperatures.length];
-        Stack<int[]> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < temperatures.length; i++) {
-            while (!stack.isEmpty() && stack.peek()[0] < temperatures[i]) {
-                int idx = stack.pop()[1];
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int idx = stack.pop();
                 answer[idx] = i - idx;
             }
-            stack.push(new int[] {temperatures[i], i});
+            stack.push(i);
         }
         return answer;
     }
