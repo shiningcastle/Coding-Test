c7b36fa1bdd572139c10f4139c0c21dd99cfa085
hs <shiningcastle.dev@gmail.com>
2021-09-07 17:11:33 +0900

    Create [14888] 연산자 끼워넣기.java

---
+++ 백준/[14888] 연산자 끼워넣기.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+    private static int max = -1000000000, min = 1000000000;
+    private void findMaxMin(int n, int[] nums, int[] operators, int idx, int sum) {
+        // 식이 완성된 경우 해당 식의 결과값으로 최대값, 최소값 갱신 후 종료
+        if (idx == n) {
+            max = Math.max(max, sum);
+            min = Math.min(min, sum);
+            return;
+        }
+        for (int i = 0; i < operators.length; i++) {
+            // 해당 연산 기호를 모두 소진한 경우 스킵
+            if (operators[i] <= 0)
+                continue;
+            // 연산한 것을 다시 되돌리기 어려우므로 값을 복사해서 연산 후 전달하여 복구 안해도 된다
+            int tmp = sum;
+            switch (i) {
+                case 0:
+                    tmp += nums[idx];
+                    break;
+                case 1:
+                    tmp -= nums[idx];
+                    break;
+                case 2:
+                    tmp *= nums[idx];
+                    break;
+                case 3:
+                    // 0으로 나누는 경우는 불가능이라 해당 연산 케이스 종료
+                    if (nums[idx] == 0)
+                        return;
+                    // 음수를 양수로 나눌 때
+                    else if (tmp < 0 && nums[idx] > 0)
+                        tmp = -((-tmp) / nums[idx]);
+                    // 나머지 경우
+                    else
+                        tmp /= nums[idx];
+            }
+            // 해당 연산자 사용 표시 후에 다음 연산자와 숫자 넣도록 재귀호출, 다시 해당 연산자 미사용 처리
+            operators[i]--;
+            findMaxMin(n, nums, operators, idx+1, tmp);
+            operators[i]++;
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        // String으로 입력된 배열의 원소들을 int로 바꾼 배열 생성
+        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] operators = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        br.close();
+        Main main = new Main();
+        main.findMaxMin(n, nums, operators, 1, nums[0]);
+        System.out.println(max);
+        System.out.print(min);
+    }
+}
