875cf49d264c70f627be0b6c887e2d3e192336bf
hs <shiningcastle.dev@gmail.com>
2021-09-08 22:30:03 +0900

    Update [1182] 부분수열의 합.java

--- 백준/[1182] 부분수열의 합.java
+++ 백준/[1182] 부분수열의 합.java
@@ -7,23 +7,23 @@ import java.util.StringTokenizer;
 class Main {
     private static int answer = 0;
 
-    private void dfs(int n, int s, int[] arr, int idx, int sum, boolean used) {
-        // 총합이 정답과 같은 경우 카운트, used : 전 순서까지의 수열이 정답인데 이번 순서의 숫자를 사용안해서 중복 정답되는 경우 방지
-        if (s == sum && used)
-            answer++;
-        // 수열의 마지막 숫자까지 사용해 만든 케이스를 위 조건문에서 체크했으므로 종료
-        if (idx == n-1)
+    private void dfs(int n, int s, int[] arr, int idx, int sum) {
+        // 해당 경우의 수의 숫자들이 완성되면 총합이 정답과 같은 경우 카운트,
+        if (idx == n-1) {
+            if (sum == s)
+                answer++;
             return;
+        }
         // 해당 인덱스 숫자 택하지 않은 경우
-        dfs(n, s, arr, idx+1, sum, false);
+        dfs(n, s, arr, idx+1, sum);
         // 해당 인덱스 숫자 택한 경우
-        dfs(n, s, arr, idx+1, sum + arr[idx+1], true);
+        dfs(n, s, arr, idx+1, sum + arr[idx+1]);
     }
 
     private void subSequence(int n, int s, int[] arr) {
         // 첫번째 원소 경우의 수에 포함 안한 경우 & 포함한 경우로 시작
-        dfs(n, s, arr, 0, 0, false);
-        dfs(n, s, arr, 0, arr[0], true);
+        dfs(n, s, arr, 0, 0);
+        dfs(n, s, arr, 0, arr[0]);
     }
 
     public static void main(String[] args) throws IOException {
@@ -34,6 +34,7 @@ class Main {
         int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         br.close();
         main.subSequence(n, s, arr);
-        System.out.print(answer);
+        // s가 0이고 모든 수열 숫자 사용안하고 sum이 0인 경우는 정답이 아니지만 카운트 되므로 제외
+        System.out.print((s == 0) ? answer-1 : answer);
     }
 }
